853cc540bf2f0a5edba389380093dc17a2165cb3
Josemi Gonzalez <xemy.013@gmail.com>
2017-11-27 22:28:14 +0100

    Arreglo a Layout 4

--- app/src/main/java/com/example/uno/practicaandroid/FichaPeliculas.java
+++ app/src/main/java/com/example/uno/practicaandroid/FichaPeliculas.java
@@ -1,16 +1,24 @@
 package com.example.uno.practicaandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class FichaPeliculas extends AppCompatActivity {
     Pelicula[] info = new Pelicula[5];
     int posicion;
+    TextView lblsinopsis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ficha_peliculas);
         posicion = getIntent().getIntExtra("posicion",0);
         info= (Pelicula[]) getIntent().getSerializableExtra("pelicula");
+        inicializarUI();
+        lblsinopsis.setMovementMethod(new ScrollingMovementMethod());
+    }
+    public void inicializarUI() {
+     lblsinopsis = (TextView) findViewById(R.id.lbl4SinopsisPelicula);
     }
 }
