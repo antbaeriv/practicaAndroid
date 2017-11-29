363a6b1e80a7e3b205b44e96804a67993a234d0e
Josemi Gonzalez <xemy.013@gmail.com>
2017-11-29 13:51:20 +0100

    Creacion del activity y layout "Metodo Pago"

---
+++ app/src/main/java/com/example/uno/practicaandroid/MetodoPago.java
@@ -0,0 +1,13 @@
+package com.example.uno.practicaandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MetodoPago extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.metodo_pago);
+    }
+}
