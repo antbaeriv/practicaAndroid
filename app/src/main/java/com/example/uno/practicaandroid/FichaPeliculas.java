0e808f260e0f86435bce5d4f0e9c094eddbd4359
Josemi Gonzalez <xemy.013@gmail.com>
2017-11-30 13:14:46 +0100

    codigo activity 4

--- app/src/main/java/com/example/uno/practicaandroid/FichaPeliculas.java
+++ app/src/main/java/com/example/uno/practicaandroid/FichaPeliculas.java
@@ -3,12 +3,16 @@ package com.example.uno.practicaandroid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FichaPeliculas extends AppCompatActivity {
     Pelicula[] info = new Pelicula[5];
     int posicion;
-    TextView lblsinopsis;
+    TextView lblSinopsis, lblTitulo, lblDirector, lblPais, lblAnio;
+    ImageView imgPelicula;
+    ImageButton atras, home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +20,23 @@ public class FichaPeliculas extends AppCompatActivity {
         posicion = getIntent().getIntExtra("posicion",0);
         info= (Pelicula[]) getIntent().getSerializableExtra("pelicula");
         inicializarUI();
-        lblsinopsis.setMovementMethod(new ScrollingMovementMethod());
+        lblSinopsis.setMovementMethod(new ScrollingMovementMethod());
+        lblSinopsis.setText(info[posicion].getSinopsis());
+        lblTitulo.setText(info[posicion].getTitulo());
+        lblAnio.setText(info[posicion].getAnio());
+        lblDirector.setText(info[posicion].getDirector());
+        lblPais.setText(info[posicion].getNacionalidad());
+        imgPelicula.setImageResource(info[posicion].getImagen());
+        atras = (ImageButton)findViewById(R.id.iBtn4Back);
+        home = (ImageButton)findViewById(R.id.iBtn4Home);
     }
     public void inicializarUI() {
-     lblsinopsis = (TextView) findViewById(R.id.lbl4SinopsisPelicula);
+     lblSinopsis = (TextView) findViewById(R.id.lbl4SinopsisPelicula);
+     lblTitulo = (TextView) findViewById(R.id.lbl4TituloPelicula);
+     lblDirector = (TextView) findViewById(R.id.lbl4TituloPelicula);
+     lblPais = (TextView) findViewById(R.id.lbl4TituloPelicula);
+     lblAnio = (TextView) findViewById(R.id.lbl4TituloPelicula);
+     imgPelicula = (ImageView) findViewById(R.id.img4Pelicula);
+
     }
 }
