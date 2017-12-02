5a7a58fc464c285158e44570a606a2873ef935c3
UNO <a.baena@andaluciajunta.es>
2017-12-02 13:31:57 +0100

    Desarrollo de parte del codigo de los metodos de pago (Actividad 5)

--- app/src/main/java/com/example/uno/practicaandroid/MetodoPago.java
+++ app/src/main/java/com/example/uno/practicaandroid/MetodoPago.java
@@ -1,13 +1,42 @@
 package com.example.uno.practicaandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MetodoPago extends AppCompatActivity {
+    TextView lblProducto;
+    TextView lblPrecio;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.metodo_pago);
+        inicializarUI();
+
+    }
+
+
+    public void inicializarUI(){
+        lblPrecio = (TextView) findViewById(R.id.lbl5Precio);
+        lblProducto = (TextView) findViewById(R.id.lbl5PeliculaAlquilada);
+    }
+
+    public void mostrarInicio(View v){
+        Intent i = new Intent(getApplicationContext(),Portada.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
+    }
+
+    public void mostrarFinalizacionCompra(View v){
+
+    }
+
+    public void mostrarListaPeliculas(View v){
+        onBackPressed();
     }
 }
